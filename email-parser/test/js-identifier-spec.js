ff8351a1bc60cde8e68917b2713c00ad6b6ea83a
Joy <d4f0b@ugrad.cs.ubc.ca>
2018-11-05 15:20:20 -0800

    cleaned up test structure

--- email-parser/test/js-identifier-spec.js
+++ email-parser/test/js-identifier-spec.js
@@ -33,77 +33,6 @@ describe("JS Parser", () => {
 
 });
 
-describe("Domain Parser", () => {
-    it("should get domains from links that start with http://", () => {
-        const httpLink = "http://user:pass@google.com/?a=b#asdd";
-        const actualHttpLink = "google.com";
-        let response = JsIdentifier.parseDomain(httpLink);
-        expect(response).to.equal(actualHttpLink);
-    });
-    it("should get domains from links that start with https://", () => {
-        const httpsLink = "https://www.compzets.com?asd=10";
-        const acutalHttpsLink = "compzets.com";
-        let response = JsIdentifier.parseDomain(httpsLink);
-        expect(response).to.equal(acutalHttpsLink);
-    });
-    it("should get domains from links that start with www.", () => {
-        const wwwLink = "www.google.co.in/sadfask/";
-        const actualwwwLink = "google.co.in";
-        let response = JsIdentifier.parseDomain(wwwLink);
-        expect(response).to.equal(actualwwwLink);
-    });
-    it("should get domains from emails", () => {
-        const emailAddress = "happydoggog@gmail.com";
-        const actual = "gmail.com";
-        let response = JsIdentifier.parseDomain(emailAddress);
-        expect(response).to.equal(actual);
-    });
-});
-
-describe("Href Parser", () => {
-   it("should parse a list of urls out of '<a href='", () => {
-       const email = 'fake email with http://text.base.url and https://www.text.base.url' +
-           'and <a href="www.links.com">test</a>' +
-           'with different formats such as ' +
-           '<a href="www.formatwithapppend.ca?dsd=fdsad/gsda">test</a>' +
-           'and <a href="http://based.io">test</a>';
-       let response = JsIdentifier.parseHrefs(email);
-       let hrefList =  [
-           'href="www.links.com"',
-           'href="www.formatwithapppend.ca?dsd=fdsad/gsda"',
-           'href="http://based.io"'
-       ];
-       expect(response).to.deep.equal(hrefList);
-       expect(response).to.deep.contains(hrefList[0]);
-       expect(response.length).to.equal(3);
-   });
-
-   it("should parse the urls out even if href is not the first element in the tag", () => {
-       const email = '<a title="Resolve" class="aapl-link" style="TEXT-DECORATION: none; FONT-WEIGHT: bold; COLOR: #ffffff; TEXT-ALIGN: center; LETTER-SPACING: normal; LINE-HEIGHT: 100%" href="http://ow.ly/KahG30mqCQX" target="_blank"><font style="FONT-SIZE: 0px; COLOR: transparent; DISPLAY: inline">8sy</font>Verify your Account</a>';
-       let response = JsIdentifier.parseHrefs(email);
-       let hrefList = ['href="http://ow.ly/KahG30mqCQX"'];
-       expect(response).to.deep.equal(hrefList);
-       expect(response.length).to.equal(1);
-   });
-
-});
-
-describe("Url Parser", () => {
-   it("should parse a url out of a href tag", () =>{
-       const href = '<a href="www.formatwithapppend.ca?dsd=fdsad/gsda">';
-       let response = JsIdentifier.parseUrl(href);
-       let actual = "www.formatwithapppend.ca?dsd=fdsad/gsda";
-       expect(response).to.deep.equal(actual);
-   });
-
-   it("should parse a url out of href tag with href not being the first element", () => {
-       const href = 'href="http://ow.ly/KahG30mqCQX" target="_blank"><font style="FONT-SIZE: 0px; COLOR: transparent; DISPLAY: inline">';
-       let response = JsIdentifier.parseUrl(href);
-       let actual = "http://ow.ly/KahG30mqCQX";
-       expect(response).to.equal(actual);
-   });
-});
-
 describe("Dots Parser", () => {
     it("should return the number of dots inside domain", () =>{
         const oneDot = "google.co";
@@ -118,44 +47,112 @@ describe("Dots Parser", () => {
     });
 });
 
-describe("Plain Url Parser", () => {
-   it("should return a list of plaintext url with href appended from an email", () => {
-       const email = 'fake email with http://text.base.url/fdsf and https://www.dsadb.f.url?f www.sthsth.com and <a href="www.links.com">test</a>';
-       let urlList = [
-           '<a href="http://text.base.url/fdsf">',
-           '<a href="https://www.dsadb.f.url?f">',
-           '<a href="www.sthsth.com">',
-       ];
-       let response = JsIdentifier.parsePlainUrls(email);
-       expect(response).to.deep.equal(urlList);
-   });
-});
-
-describe("Find Domain List", () => {
-   it("should find a list of domains from an email", () => {
-       const email = 'fake email with http://text.base.url and https://www.dsadb.f.url' +
-           'and <a href="www.links.com">test</a>' +
-           'with different formats such as ' +
-           '<a href="www.formatwithapppend.ca?dsd=fdsad/gsda">test</a>' +
-           'and <a href="http://based.io">test</a>' +
-           'and <a href="https://basedsdd.ca">test</a>' +
-           'and <a href="https://www.324-fdsfa.dd.ca">test</a>';
-       let domainList = [
-           "text.base.url",
-           "www.dsadb.f.url",
-           "links.com",
-           "formatwithapppend.ca",
-           "based.io",
-           "basedsdd.ca",
-           "324-fdsfa.dd.ca"
-       ];
-       let response = JsIdentifier.findDomainList(email);
-       expect(response).to.include("formatwithapppend.ca");
-       expect(response.length).to.equal(7);
-   });
-});
-
 describe("numOfDomainMisMatch", () => {
+    describe("Domain Parser", () => {
+        it("should get domains from links that start with http://", () => {
+            const httpLink = "http://user:pass@google.com/?a=b#asdd";
+            const actualHttpLink = "google.com";
+            let response = JsIdentifier.parseDomain(httpLink);
+            expect(response).to.equal(actualHttpLink);
+        });
+        it("should get domains from links that start with https://", () => {
+            const httpsLink = "https://www.compzets.com?asd=10";
+            const acutalHttpsLink = "compzets.com";
+            let response = JsIdentifier.parseDomain(httpsLink);
+            expect(response).to.equal(acutalHttpsLink);
+        });
+        it("should get domains from links that start with www.", () => {
+            const wwwLink = "www.google.co.in/sadfask/";
+            const actualwwwLink = "google.co.in";
+            let response = JsIdentifier.parseDomain(wwwLink);
+            expect(response).to.equal(actualwwwLink);
+        });
+        it("should get domains from emails", () => {
+            const emailAddress = "happydoggog@gmail.com";
+            const actual = "gmail.com";
+            let response = JsIdentifier.parseDomain(emailAddress);
+            expect(response).to.equal(actual);
+        });
+    });
+
+    describe("Href Parser", () => {
+        it("should parse a list of urls out of '<a href='", () => {
+            const email = 'fake email with http://text.base.url and https://www.text.base.url' +
+                'and <a href="www.links.com">test</a>' +
+                'with different formats such as ' +
+                '<a href="www.formatwithapppend.ca?dsd=fdsad/gsda">test</a>' +
+                'and <a href="http://based.io">test</a>';
+            let response = JsIdentifier.parseHrefs(email);
+            let hrefList =  [
+                'href="www.links.com"',
+                'href="www.formatwithapppend.ca?dsd=fdsad/gsda"',
+                'href="http://based.io"'
+            ];
+            expect(response).to.deep.equal(hrefList);
+            expect(response).to.deep.contains(hrefList[0]);
+            expect(response.length).to.equal(3);
+        });
+
+        it("should parse the urls out even if href is not the first element in the tag", () => {
+            const email = '<a title="Resolve" class="aapl-link" style="TEXT-DECORATION: none; FONT-WEIGHT: bold; COLOR: #ffffff; TEXT-ALIGN: center; LETTER-SPACING: normal; LINE-HEIGHT: 100%" href="http://ow.ly/KahG30mqCQX" target="_blank"><font style="FONT-SIZE: 0px; COLOR: transparent; DISPLAY: inline">8sy</font>Verify your Account</a>';
+            let response = JsIdentifier.parseHrefs(email);
+            let hrefList = ['href="http://ow.ly/KahG30mqCQX"'];
+            expect(response).to.deep.equal(hrefList);
+            expect(response.length).to.equal(1);
+        });
+
+    });
+
+    describe("Url Parser", () => {
+        it("should parse a url out of a href tag", () =>{
+            const href = '<a href="www.formatwithapppend.ca?dsd=fdsad/gsda">';
+            let response = JsIdentifier.parseUrl(href);
+            let actual = "www.formatwithapppend.ca?dsd=fdsad/gsda";
+            expect(response).to.deep.equal(actual);
+        });
+
+        it("should parse a url out of href tag with href not being the first element", () => {
+            const href = 'href="http://ow.ly/KahG30mqCQX" target="_blank"><font style="FONT-SIZE: 0px; COLOR: transparent; DISPLAY: inline">';
+            let response = JsIdentifier.parseUrl(href);
+            let actual = "http://ow.ly/KahG30mqCQX";
+            expect(response).to.equal(actual);
+        });
+    });
+    describe("Plain Url Parser", () => {
+        it("should return a list of plaintext url with href appended from an email", () => {
+            const email = 'fake email with http://text.base.url/fdsf and https://www.dsadb.f.url?f www.sthsth.com and <a href="www.links.com">test</a>';
+            let urlList = [
+                '<a href="http://text.base.url/fdsf">',
+                '<a href="https://www.dsadb.f.url?f">',
+                '<a href="www.sthsth.com">',
+            ];
+            let response = JsIdentifier.parsePlainUrls(email);
+            expect(response).to.deep.equal(urlList);
+        });
+    });
+    describe("Find Domain List", () => {
+        it("should find a list of domains from an email", () => {
+            const email = 'fake email with http://text.base.url and https://www.dsadb.f.url' +
+                'and <a href="www.links.com">test</a>' +
+                'with different formats such as ' +
+                '<a href="www.formatwithapppend.ca?dsd=fdsad/gsda">test</a>' +
+                'and <a href="http://based.io">test</a>' +
+                'and <a href="https://basedsdd.ca">test</a>' +
+                'and <a href="https://www.324-fdsfa.dd.ca">test</a>';
+            let domainList = [
+                "text.base.url",
+                "www.dsadb.f.url",
+                "links.com",
+                "formatwithapppend.ca",
+                "based.io",
+                "basedsdd.ca",
+                "324-fdsfa.dd.ca"
+            ];
+            let response = JsIdentifier.findDomainList(email);
+            expect(response).to.include("formatwithapppend.ca");
+            expect(response.length).to.equal(7);
+        });
+    });
     it("should return the number of mismatched domains", () => {
         let domainList = [
             "text.base.url",
@@ -171,6 +168,7 @@ describe("numOfDomainMisMatch", () => {
         expect(response).to.equal(6);
     });
 });
+
 describe("numOfLinks", () => {
     it("should return the number of links in the email", () => {
         const email = 'fake email with http://text.base.url and https://www.dsadb.f.url' +
