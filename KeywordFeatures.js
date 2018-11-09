7bfeba1e685cdf61f038549b97aa9bf0e7580983
Liang Nie <liangnie0127@gmail.com>
2018-11-09 23:38:17 +0000

    Update KeywordFeatures.js

--- KeywordFeatures.js
+++ KeywordFeatures.js
@@ -1,22 +1,6 @@
 var exports = module.exports = {};
 
-if(emailBody === "")
-{
-    exports.emptyCheck = true;
-    export.nullCheck = false;
-    console.log('The email body string passed in has no content.');
-}
-else if(emailBody == null)
-{
-    exports.emptyCheck = false;
-    export.nullCheck = true;
-    console.log('The email body string passed in points to NULL.');
-}
-else
-{
-    exports.emptyCheck = false;
-    export.nullCheck = false;
-    
+
     /*
      * Calculate the word count and normalisation of the features
      *   of six sets of keywords.
@@ -28,8 +12,47 @@ else
      *		        featureNormalization: Normalisations of six keyword sets.
      */
     
-    exports.keywordFeatures = function(emailBody) {
+exports.KeywordFeatures = function(emailBody) {
     
+    var emptyCheck;
+    var nullCheck;
+    var features = new Array(6);
+	var featureWordCount = new Array(6);
+	var featureNormalization = new Array(6);
+
+    if(emailBody === "")
+    {
+        emptyCheck = true;
+        nullCheck = false;
+        console.log('The email body string passed in has no content.');
+    
+        for (i=0 ; i < 6 ; i++)
+        {
+        	features[i]="";
+		    featureWordCount[i]=0;
+		    featureNormalization[i]=0;
+    	}
+    }
+    
+    else if(emailBody == null)
+    {
+        emptyCheck = false;
+        nullCheck = true;
+        console.log('The email body string passed in points to NULL.');
+    
+        for (i=0 ; i < 6 ; i++)
+        {
+            features[i]=null;
+        	featureWordCount[i]=0;
+    		featureNormalization[i]=0;
+    	}
+    }
+    
+    else
+    {
+        emptyCheck = false;
+        nullCheck = false;
+        
     	// Array of keywords
     	var keywords = new Array("update", "confirm", "user", "customer", "client", "suspend", "restrict", "hold", "verify",
     		"account", "notif", "login", "username", "password", "click", "log", "SSN", "social security", "secur", "inconvenien");
@@ -38,11 +61,11 @@ else
     	var normalizations = new Array(20);
     
     	// Replace '\n' with space ' ' 
-        emailStr = emailStr.replace("\n", " ");
+        emailBody = emailBody.replace("\n", " ");
     
     	// Count the word
     	var count = 0;
-    	words = emailStr.split(" "); 
+    	words = emailBody.split(" "); 
     	for (i=0 ; i < words.length ; i++){
        	// inner loop -- do the count
        	if (words[i] != "")
@@ -50,56 +73,53 @@ else
     	}
     
     	// Convert all letters to lower-case
-    	emailStr = emailStr.toLowerCase();
+    	emailBody = emailBody.toLowerCase();
     
     	// Count the keywords
-    	counts[0] = (emailStr.match(/update/g) || []).length;
+    	counts[0] = (emailBody.match(/update/g) || []).length;
     
-        counts[1] = (emailStr.match(/confirm/g) || []).length;
+        counts[1] = (emailBody.match(/confirm/g) || []).length;
     
-    	counts[2] = (emailStr.match(/user/g) || []).length;
+    	counts[2] = (emailBody.match(/user/g) || []).length;
     
-    	counts[3] = (emailStr.match(/customer/g) || []).length;
+    	counts[3] = (emailBody.match(/customer/g) || []).length;
     
-    	counts[4] = (emailStr.match(/client/g) || []).length;
+    	counts[4] = (emailBody.match(/client/g) || []).length;
     
-    	counts[5] = (emailStr.match(/suspend/g) || []).length;
+    	counts[5] = (emailBody.match(/suspend/g) || []).length;
         	
-    	counts[6] = (emailStr.match(/restrict/g) || []).length;
+    	counts[6] = (emailBody.match(/restrict/g) || []).length;
     
-    	counts[7] = (emailStr.match(/hold/g) || []).length;
+    	counts[7] = (emailBody.match(/hold/g) || []).length;
     
-    	counts[8] = (emailStr.match(/verify/g) || []).length;
+    	counts[8] = (emailBody.match(/verify/g) || []).length;
     
-    	counts[9] = (emailStr.match(/account/g) || []).length;
+    	counts[9] = (emailBody.match(/account/g) || []).length;
     
-    	counts[10] = (emailStr.match(/notif/g) || []).length;
+    	counts[10] = (emailBody.match(/notif/g) || []).length;
     
-    	counts[11] = (emailStr.match(/login/g) || []).length;
+    	counts[11] = (emailBody.match(/login/g) || []).length;
     
-    	counts[12] = (emailStr.match(/username/g) || []).length;
+    	counts[12] = (emailBody.match(/username/g) || []).length;
     
-    	counts[13] = (emailStr.match(/password/g) || []).length;
+    	counts[13] = (emailBody.match(/password/g) || []).length;
     
-    	counts[14] = (emailStr.match(/click/g) || []).length;
+    	counts[14] = (emailBody.match(/click/g) || []).length;
     
-    	counts[15] = (emailStr.match(/log/g) || []).length;
+    	counts[15] = (emailBody.match(/log/g) || []).length;
     
-    	counts[16] = (emailStr.match(/ssn/g) || []).length;
+    	counts[16] = (emailBody.match(/ssn/g) || []).length;
     
-    	counts[17] = (emailStr.match(/social security/g) || []).length;
+    	counts[17] = (emailBody.match(/social security/g) || []).length;
     
-    	counts[18] = (emailStr.match(/secur/g) || []).length;
+    	counts[18] = (emailBody.match(/secur/g) || []).length;
     
-    	counts[19] = (emailStr.match(/inconvenien/g) || []).length;
+    	counts[19] = (emailBody.match(/inconvenien/g) || []).length;
     
     	for(i=0; i < 20; i++){
     		normalizations[i] = counts[i]/count;
     	}
     
-        var features = new Array(6);
-    	var featureWordCount = new Array(6);
-    	var featureNormalization = new Array(6);
     
         // Feature 1: Update; Confirm
         features[0] = keywords[0] + "; " + keywords[1];
@@ -130,12 +150,13 @@ else
         features[5] = keywords[16] + "; " + keywords[17] + "; " + keywords[18] + "; " + keywords[19];
     	featureWordCount[5] = counts[16] + counts[17] + counts[18] + counts[19]; 
     	featureNormalization[5] = normalizations[16] + normalizations[17] + normalizations[18] + normalizations[19];
-    
-    	return {
-    		features: features,
-    		featureWordCount: featureWordCount,
-    		featureNormalization: featureNormalization
-    	};
-    
     }
+    
+    return {
+		emptyCheck: emptyCheck,
+		nullCheck: nullCheck,
+		features: features,
+		featureWordCount: featureWordCount,
+    	featureNormalization: featureNormalization
+	};
 }
